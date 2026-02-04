1ce6976f0ad04dfe699a3e6d68594665f2681645
Villegazs <avmorales2210@gmail.com>
2026-02-04 15:39:22 -0500

    Add CLI menu and update .gitignore
    Add initial Cryptocurrency_Exchange/main.cpp implementing a simple CLI skeleton that prints menu options (1-6) and reads a user selection. Update .gitignore to ignore the build artifact Cryptocurrency_Exchange/main.s.

---
+++ Cryptocurrency_Exchange/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main(){
+
+    // 1 print help
+    std::cout << "1. Print Help" << std::endl;
+    // 2 print exchange stats
+    std::cout << "2. Print Exchange Stats" << std::endl;
+    // 3 make an offer
+    std::cout << "3. Make an Offer" << std::endl;
+    // 4 make a bid
+    std::cout << "4. Make a Bid" << std::endl;
+    // 5 print wallet
+    std::cout << "5. Print Wallet" << std::endl;
+    // 6 continue
+    std::cout << "6. Continue" << std::endl;
+
+    std::cout << "Select an option: " << std::endl;
+    std::cout << "====================" << std::endl;
+    std::cout << "Type in 1-6" << std::endl;
+    
+    int userOption;
+    std::cin >> userOption;
+    std:: cout << "You chose: " << userOption << std::endl;
+
+    return 0;
+}
