0209f6fdf7a3a8c83676d97f4c873601758320d6
Villegazs <avmorales2210@gmail.com>
2026-02-20 18:01:28 -0500

    Add OrderBook header; remove btc-usd-max.csv
    Introduce OrderBook.h defining an OrderBook class that wraps CSV reading (constructor takes a CSV filename) and exposes getKnownProducts() and getOrders(type, product, timestamp). Includes OrderBookEntry and CSVReader headers. Also remove the large btc-usd-max.csv dataset file (cleanup to reduce repo size / move data out of source tree).

---
+++ Merklerex/OrderBook.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "OrderBookEntry.h"
+#include "CSVReader.h"
+#include <string>
+#include <vector>
+
+class OrderBook
+{
+public:
+	/*Construct reading a csv data file*/
+	OrderBook(std::string csvFileName);
+	/*Return vector of all known products in dataset*/
+	std::vector<std::string> getKnownProducts();
+	/*Return vector of all orders for a given product and timestamp*/
+	std::vector<OrderBookEntry> getOrders(OrderBookType type, 
+										std::string product, 
+										std::string timestamp);
+};
